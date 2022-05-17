8d90b884306bb3528355a8b9897077e8ddc4e8e6
Nguyễn Minh Duy <35254204+toithichlaptrinh@users.noreply.github.com>
2022-05-18 00:19:20 +0700

    Refactor UI Component Admin App
    Refactor UI Component Admin App

--- admin-app/src/app/authentication/authentication-routing.module.ts
+++ admin-app/src/app/authentication/authentication-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-import { Error1Component } from './error-1/error-1.component';
-import { Error2Component } from './error-2/error-2.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
@@ -11,20 +8,6 @@ const routes: Routes = [
         component: LoginComponent,
         data: {
             title: 'Login'
-        }
-    },
-    {
-        path: 'error-1',
-        component: Error1Component,
-        data: {
-            title: 'Error 1'
-        }
-    },
-    {
-        path: 'error-2',
-        component: Error2Component,
-        data: {
-            title: 'Error 2'
         }
     }
 ];
