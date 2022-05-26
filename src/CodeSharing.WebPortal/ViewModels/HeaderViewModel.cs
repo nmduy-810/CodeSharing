931ca9f89959f488c44233a1223a87a49e1d502a
Nguyễn Minh Duy <35254204+toithichlaptrinh@users.noreply.github.com>
2022-05-26 23:25:24 +0700

    Delete canvas menu and completed edit user
    Delete canvas menu and completed edit user

--- src/CodeSharing.WebPortal/ViewModels/HeaderViewModel.cs
+++ src/CodeSharing.WebPortal/ViewModels/HeaderViewModel.cs
@@ -1,8 +1,11 @@
 using CodeSharing.ViewModels.Contents.Category;
+using CodeSharing.ViewModels.Systems.User;
 
 namespace CodeSharing.WebPortal.ViewModels;
 
 public class HeaderViewModel
 {
     public List<CategoryVm> Categories { get; set; }
+    public UserVm CurrentUser { get; set; }
+    
 }
