ea34b4aa564351d6109651e97ce8de676f63ee58
Nguyễn Minh Duy <35254204+toithichlaptrinh@users.noreply.github.com>
2023-03-04 12:53:45 +0700

    Intergrate sweetalert 2

---
+++ WebPortal/CodeSharing.Portal/ViewModels/WidgetCategoryViewModel.cs
@@ -0,0 +1,8 @@
+using CodeSharing.DTL.Models.Contents.Post;
+
+namespace CodeSharing.Portal.ViewModels;
+
+public class WidgetCategoryViewModel
+{
+    public List<PostQuickVm> ExplorerTopics { get; set; }  = default!;
+}
